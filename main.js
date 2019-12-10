8090535163900fab46967701ab8845ea4195feac
Harley <Zonkeystripes@hotmail.com>
2019-12-10 12:23:02 -0700

    Displays basic weather

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 
-
-
-let apiKey = "524901&APPID=828d8c5dd2b3ef0673d597d675ed89f6"
+let apiKey = "828d8c5dd2b3ef0673d597d675ed89f6"
+let city = "Tucson"
 var title = "";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=" + apiKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + apiKey;
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
     console.log(queryURL)
+
+    // this logs the object of the url search
+    console.log(response)
+
+    console.log(response.city.name)//city name
+    $(".city").html("<h2>" + response.city.name + " Weather")
+
+    console.log(response.list[0].main.temp)//temp
+    // $(".temp").html("<p>" + "Average temperature: " + response.list[0].main.temp)
+    var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
+    $(".temp").text("Average temperature: " + Math.round(tempF) + "°F");
+    
+    console.log(response.list[0].main.humidity)//humidity
+    $(".humidity").text("Humidity: " + response.list[0].main.humidity)
+
+    console.log(response.list[0].wind)//wind speed
+    $(".wind").text("Avg Wind Speed: " + response.list[0].wind.speed + "Mph")
+
+    console.log(response.city.country)//uv index
+
+
+
 });
 
 
